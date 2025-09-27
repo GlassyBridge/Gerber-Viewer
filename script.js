915c46eaef915dbbbc72a31118a3b30b339b067a
GlassyBridge114 <188959481+GlassyBridge@users.noreply.github.com>
2025-09-27 13:35:58 +0300

    Migrated the previous logic to the new one

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 import {read, plot, renderLayers, renderBoard} from '@tracespace/core';
 import svgPanZoom from 'svg-pan-zoom';
-// import { parse } from './OLD/parser.js';
-// import { getLayers } from './OLD/getLayers.js';
-// import { renderLayer} from "./OLD/renderLayer.js";
+import { renderSvg } from './renderSVG.js';
+import { parse } from './tst/parser.js';
+import { getLayers } from './tst/getLayers.js';
+import { renderLayer } from "./tst/renderLayer.js";
 
 const gerberInput = document.getElementById('gerber-input');
 const svg = document.getElementById('svg');
@@ -11,50 +12,103 @@ const viewSettings = svgPanZoom('#svg', {
     preventMouseEventsDefault: false,
     minZoom: 0.75,
     maxZoom: 100,
-});
+    beforePan: function (oldPan, newPan){
+                    const stopHorizontal = false
+                    , stopVertical = false
+                    , gutterWidth = 100
+                    , gutterHeight = 100
+                    , sizes = this.getSizes()
+                    , leftLimit = -((sizes.viewBox.x + sizes.viewBox.width) * sizes.realZoom) + gutterWidth
+                    , rightLimit = sizes.width - gutterWidth - (sizes.viewBox.x * sizes.realZoom)
+                    , topLimit = -((sizes.viewBox.y + sizes.viewBox.height) * sizes.realZoom) + gutterHeight
+                    , bottomLimit = sizes.height - gutterHeight - (sizes.viewBox.y * sizes.realZoom)
 
+                    const customPan = {}
+                    customPan.x = Math.max(leftLimit, Math.min(rightLimit, newPan.x))
+                    customPan.y = Math.max(topLimit, Math.min(bottomLimit, newPan.y))
+
+                    return customPan
+                }
+});
 const viewPort = document.querySelector('.svg-pan-zoom_viewport');
+
+let renderMethod = 'top';
+let renderBoardResult;
+let layers;
+
+let topLayer = document.createElementNS("http://www.w3.org/2000/svg", 'g');
+topLayer.id = 'topLayer';
+let bottomLayer = document.createElementNS("http://www.w3.org/2000/svg", 'g');
+bottomLayer.id = 'bottomLayer';
+let allLayers = document.createElementNS("http://www.w3.org/2000/svg", 'g');
+allLayers.id = 'allLayers';
+
+const bt1 = document.getElementById('bt1');
+bt1.addEventListener('click', () => {
+    renderMethod = 'top';
+
+    topLayer.classList.remove('hidden');
+    bottomLayer.classList.add('hidden');
+    allLayers.classList.add('hidden');
+
+    viewSettings.updateBBox();
+    // viewSettings.fit();
+    // viewSettings.center();
+});
+const bt2 = document.getElementById('bt2');
+bt2.addEventListener('click', () => {
+    renderMethod = 'bottom';
+
+    topLayer.classList.add('hidden');
+    bottomLayer.classList.remove('hidden');
+    allLayers.classList.add('hidden');
+
+    viewSettings.updateBBox();
+    viewSettings.fit();
+    viewSettings.center();
+});
+const bt3 = document.getElementById('bt3');
+bt3.addEventListener('click', () => {
+    renderMethod = 'layers';
+    
+    topLayer.classList.add('hidden');
+    bottomLayer.classList.add('hidden');
+    allLayers.classList.remove('hidden');
+
+    viewSettings.updateBBox();
+    // viewSettings.fit();
+    // viewSettings.center();
+});
 
 gerberInput.addEventListener('change', async e => {
     const files = Array.from(e.target.files);
-    const fileNames = [];
 
     if (files.length > 0) {
         const readResult = await read(files);
         const plotResult = plot(readResult);
         const renderLayersResult = renderLayers(plotResult);
-        const renderBoardResult = renderBoard(renderLayersResult);
+        renderBoardResult = renderBoard(renderLayersResult);
 
-        console.log(renderLayersResult);
+        const parsedCommands = parse(readResult);
+        layers = getLayers(parsedCommands);
 
-        // await Promise.all([
-        //     // vol.writeFile('top.svg', renderBoardResult.top, catchErr(err)),
-        //     // vol.writeFile('bottom.svg', renderBoardResult.bottom, catchErr(err)),
-        //     // fs.writeFile('top.svg', renderBoardResult.top),
-        //     // fs.writeFile('bottom.svg', renderBoardResult.bottom)
-        // ]);
-        // Array.from(files).forEach(file => { fileNames.push(file.name); });
-        //
-        // const filePromises = Array.from(files).map(file => {
-        //     return new Promise(resolve => {
-        //         const reader = new FileReader();
-        //         reader.onload = e => resolve(e.target.result);
-        //         reader.readAsText(file);
-        //     });
-        // });
-        //
-        // const fileContents = await Promise.all(filePromises);
-        // const parsedCommandsArray = fileContents.map(content => {
-        //     return parse(content);
-        // });
-        //
-        // const layers = getLayers(parsedCommandsArray, fileNames);
-        // for (const layerName in layers) {
-        //     const layer = layers[layerName];
-        //     console.log(layer);
-        //     viewPort.appendChild(renderLayer(layer));
-        // }
+        topLayer.innerHTML = renderSvg(renderBoardResult.top);
+        bottomLayer.innerHTML = renderSvg(renderBoardResult.bottom);
+        for (const layerName in layers) {
+            console.log(layerName);
+            const layerData = layers[layerName];
+            const layer = renderLayer(layerData);
+            allLayers.appendChild(layer);
+        }
+        allLayers.style.transform = 'scale(1,-1)';
+        
+        viewPort.appendChild(topLayer);
+        viewPort.appendChild(bottomLayer);
+        viewPort.appendChild(allLayers);
 
+        bottomLayer.classList.add('hidden');
+        allLayers.classList.add('hidden');
+        
         viewSettings.updateBBox();
         viewSettings.fit();
         viewSettings.center();
