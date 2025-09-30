8baf0af5a8d0ec7d7b647238d469efce6c923c38
GlassyBridge114 <188959481+GlassyBridge@users.noreply.github.com>
2025-09-30 07:17:02 +0000

    Edit: Build settings

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite';
 
+const REPO_NAME = 'Gerber-Viewer'; 
+
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? `/${REPO_NAME}/` : '/',
+  
   build: {
     rollupOptions: {
       input: {
